2dd9cd4427954a146911bab2faccc0940516e871
Andrew King <luv2bking@gmail.com>
2017-02-14 08:17:23 -0700

    change for debugging

--- app.js
+++ app.js
@@ -218,7 +218,7 @@ function ensureAuthenticated(req, res, next) {
         return done(err);
              //No user was found... so create a new user with values from Facebook (all the profile. stuff)
       } else {
-          console.log(users.checkins[0]);
+          console.log(users);
           res.render('not-account', {user: users});          
       }
     });
